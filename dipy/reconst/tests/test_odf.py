6bef1494d3f5ed24e1604e74a74d8d01b22f375e
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-02-21 15:53:03 -0500

    NF: added spherical deconvolution family of functions from raw signal (Tournier et al NI 2004 and 2007)

--- dipy/reconst/tests/test_odf.py
+++ dipy/reconst/tests/test_odf.py
@@ -6,9 +6,14 @@ from dipy.core.subdivide_octahedron import create_unit_hemisphere
 from dipy.core.sphere import unit_icosahedron, unit_octahedron
 from nose.tools import (assert_almost_equal, assert_equal, assert_raises,
                         assert_true)
-from dipy.reconst.shm import sf_to_sh, sh_to_sf
+from dipy.reconst.shm import (sph_harm_ind_list, sph_harm_lookup, smooth_pinv,
+                              real_sph_harm, sh_to_sf, sf_to_sh)
 from dipy.data import get_sphere
-from dipy.sims.voxel import multi_tensor_odf
+from dipy.sims.voxel import (single_tensor, single_tensor_odf,
+                            multi_tensor, multi_tensor_odf)
+from dipy.core.gradients import gradient_table
+
+
 
 def test_peak_directions_nl():
     def discrete_eval(sphere):
@@ -199,7 +204,341 @@ def test_sf_to_sh():
     odf2d_sh = sf_to_sh(odf2d, sphere, 8)
     odf2d_sf = sh_to_sf(odf2d_sh, sphere, 8)
     assert_array_almost_equal(odf2d, odf2d_sf, 2)
+
+def test_deconv():
+    SNR = 30 #10, 20, None
+    bvalue = 1000
+    S0 = 1
+    sh_order = 8
+    visu = True
     
+    # signal gtab
+    #sphere = unit_octahedron
+    #sphere = sphere.subdivide(2)
+    sphere = get_sphere('symmetric362')
+    s_bvecs = np.concatenate(([[0, 0, 0]], sphere.vertices))
+    s_bvals = np.zeros(len(s_bvecs)) + bvalue
+    s_bvals[0] = 0
+    s_gtab = gradient_table(s_bvals, s_bvecs)    
+    s_mevals = np.array(([0.0015, 0.0003, 0.0003], [0.0015, 0.0003, 0.0003] ))
+    s_mevecs = [ np.array( [ [1,0,0], [0,1,0], [0,0,1] ] ),
+               np.array( [ [0,0,1], [0,1,0], [1,0,0] ] ) ]
+    S,sticks = multi_tensor( s_gtab, s_mevals, S0, angles=[(0, 0), (90, 0)],
+                               fractions=[50, 50], snr=SNR )
+    odf_gt = multi_tensor_odf( sphere.vertices, [0.5, 0.5], s_mevals, s_mevecs )
+
+    if visu :
+        from dipy.viz import fvtk
+        r = fvtk.ren()
+        fvtk.add( r, fvtk.sphere_funcs( np.vstack((S[1:], odf_gt)), sphere ) )
+        fvtk.show( r )
+
+
+    # single fiber response function gtab on a sphere of 362 points
+    psphere = get_sphere('symmetric362')
+    r_bvecs = np.concatenate(([[0, 0, 0]], psphere.vertices))
+    r_bvals = np.zeros(len(r_bvecs)) + bvalue
+    r_bvals[0] = 0
+    r_gtab = gradient_table(r_bvals, r_bvecs)
+    R = single_tensor( r_gtab, S0, s_mevals[1], s_mevecs[1], snr=None )
+    r_odf = single_tensor_odf( psphere.vertices, s_mevals[1], s_mevecs[1] )
+
+    if visu :
+        from dipy.viz import fvtk
+        r = fvtk.ren()
+        fvtk.add( r, fvtk.sphere_funcs( np.vstack((R[1:], r_odf)), psphere ) )
+        fvtk.show( r )
+
+    s_sh,B_dwi   = sf_to_sh2( S[1:], sphere, sh_order )
+    r_sh,B_regul = sf_to_sh2( R[1:], psphere, sh_order )
+    r_rh         = sh_to_rh( r_sh, sh_order )     
+
+    u_fodf_sh,b = sdeconv( r_rh, s_sh, sh_order, False )
+    u_fodf = sh_to_sf(u_fodf_sh, psphere, sh_order)
+
+    if visu :
+        odf_gt = multi_tensor_odf( psphere.vertices, [0.5, 0.5], s_mevals, s_mevecs )
+        from dipy.viz import fvtk
+        r = fvtk.ren()
+        fvtk.add( r, fvtk.sphere_funcs( np.vstack((odf_gt, u_fodf)), psphere ) )
+        fvtk.show( r )
+        
+    f_fodf_sh,b = sdeconv( r_rh, s_sh, sh_order, True )
+    f_fodf = sh_to_sf(f_fodf_sh, psphere, sh_order)
+    
+    if visu :
+        odf_gt = multi_tensor_odf( psphere.vertices, [0.5, 0.5], s_mevals, s_mevecs )
+        from dipy.viz import fvtk
+        r = fvtk.ren()
+        fvtk.add( r, fvtk.sphere_funcs( np.vstack((odf_gt, f_fodf)), psphere ) )
+        fvtk.show( r )
+
+    fodf_sh,num_it = csdeconv( r_rh, s_sh, sh_order, B_regul, 1.0, 0.1 )
+    fodf = sh_to_sf(fodf_sh, psphere, sh_order)
+    print 'converged afted %d iterations'%num_it
+    visu = True
+    if visu :
+        odf_gt = multi_tensor_odf( psphere.vertices, [0.5, 0.5], s_mevals, s_mevecs )
+        from dipy.viz import fvtk
+        r = fvtk.ren()
+        fvtk.add( r, fvtk.sphere_funcs( np.vstack((odf_gt, fodf)), psphere ) )
+        fvtk.show( r )
+        
+def csdeconv( r_rh, s_sh, sh_order, B_regul, Lambda, tau ) :
+    """ Unfiltered spherical deconvolution 
+
+    Deconvolves the axially symmetric single fiber response
+    function `r_rh` in rotational harmonics coefficients from the spherical function
+    `s_sh` in SH coefficients. 
+    
+    Parameters
+    ----------
+    r_rh : ndarray
+         ndarray of rotational harmonics coefficients for the single fiber response function
+    s_sh : ndarray
+         ndarray of SH coefficients for the spherical function to be deconvolved
+    sphere_regul : Sphere
+         The points on which the regularization will take place
+    Lambda : float
+         lambda parameter in minimization equation (default 1.0)
+    tau : float
+         tau parameter in the L matrix construction (default 0.1)
+    sh_order : int
+         maximal SH order of the SH representation
+         
+    Returns
+    _______
+    fodf_sh : ndarray
+         Spherical harmonics coefficients of the constrained-regularized fiber ODF
+    num_it : int
+         Number of iterations in the constrained-regularization used for convergence
+
+    References
+    ----------
+    Tournier, J.D., et. al. NeuroImage 2007.
+    """
+    m, n = sph_harm_ind_list(sh_order)
+
+    # building forward spherical deconvolution matrix
+    b = np.zeros( (m.shape) )
+    i = 0
+    for l in np.arange(0,sh_order+1,2) :
+        for m in np.arange(-l,l+1) :
+            b[i] = r_rh[l/2]                
+            i = i + 1
+    R = np.diag( b )
+
+    # generate initial fODF estimate, truncated at SH order 4
+    fodf_sh = np.linalg.lstsq(R, s_sh)[0]  # a\b
+    fodf_sh[15:] = 0    
+    
+    #set threshold on FOD amplitude used to identify 'negative' values
+    threshold = tau*np.mean(np.dot(B_regul, fodf_sh));
+    
+    # scale lambda to account for differences in the number of 
+    # SH coefficients and number of mapped directions
+    Lambda = Lambda * R.shape[0] * r_rh[0] / B_regul.shape[0]
+    if sh_order == 8 :
+        assert_almost_equal(Lambda, 0.822627111014, 4) # as in mrtrix
+
+    k = []
+    convergence = 50
+    for num_it in np.arange(1,convergence+1) :
+        A = np.dot(B_regul, fodf_sh)
+        k2 = np.nonzero( A < threshold )[0]
+        
+        if (k2.shape[0] + R.shape[0])  < B_regul.shape[1] :
+            print 'too few negative directions identified - failed to converge'
+            return fodf_sh,num_it
+    
+        if num_it > 1 and k.shape[0] == k2.shape[0] :
+            if (k == k2).all() :
+                return fodf_sh,num_it
+           
+        k = k2
+        M = np.concatenate( (R, Lambda*B_regul[k, :] ) )
+        S = np.concatenate( (s_sh, np.zeros( k.shape ) ) )
+        fodf_sh = np.linalg.lstsq(M, S)[0]  # M\S
+        
+    print 'maximum number of iterations exceeded - failed to converge';
+    return fodf_sh,num_it
+
+def sdeconv( r_rh, s_sh, sh_order, filter ) :
+    """ Unfiltered and filtered spherical deconvolution 
+
+    Deconvolves the axially symmetric single fiber response
+    function `r_rh` in rotational harmonics coefficients from
+    the spherical function `s_sh` in SH coefficients. 
+
+    If filter is True, perform 'standard' filtered
+    spherical deconvolution. Note that the
+    filtering coefficients are supplied as their reciprocals - the 
+    filter coefficients here are actually
+    [ 1 1 1 0.1 0.01 inf inf inf ] 
+    (the last four correspond to sh_order > 8 and are unused)
+
+    Parameters
+    ----------
+    r_rh : ndarray
+         ndarray of rotational harmonics coefficients for
+         the single fiber response function
+    s_sh : ndarray
+         ndarray of spherical harmonics coefficients for
+         the spherical function to deconvolve
+    sh_order : int
+         maximal SH order of the SH representation
+    filted : bool
+         Filter or not the fiber ODF coefficients output
+         
+    Returns
+    _______
+    fodf_sh : ndarray
+         Spherical harmonics coefficients of the unfiltered fiber ODF
+
+    References
+    ----------
+    Tournier, J.D., et. al. NeuroImage 2004.
+    """
+    
+    m, n = sph_harm_ind_list(sh_order)
+    # other bases support TO DO
+
+    if filter :
+        f = np.zeros( (r_rh.shape) )
+        f[:5] = np.array([1, 1, 1, 2, 10])
+        r_rh = r_rh*f
+    
+    b = np.zeros( (m.shape) )
+    i = 0
+    for l in np.arange(0,sh_order+1,2) :
+        for m in np.arange(-l,l+1) :
+            b[i] = 1/r_rh[l/2]                
+            i = i + 1
+
+    fodf_sh = b * s_sh        
+    return fodf_sh,b
+
+
+def test_b() :
+    # this values come from mrtrix
+    r_rh = np.array( [1.7450, -0.6124, 0.2205, -0.0657, 0.01622] )
+    b = np.array( [0.5731, -1.6329, 4.5345, -15.2235, 61.6530 ] )    
+    fodf_sh,b_bis = usdeconv(r_rh, r_rh, 8)
+    k = np.array( [0, 3, 10, 21, 36] )
+    assert_array_almost_equal(b, b_bis[k], 2 )
+    
+    
+def sh_to_rh( r_sh, sh_order ) :
+    """ Spherical harmonics (SH) to rotational harmonics (RH)
+
+    Calculate the rotational harmonic decomposition up to 
+    harmonic sh_order for an axially and antipodally 
+    symmetric function. Note that
+    all m != 0 coefficients will be ignored as axial symmetry
+    is assumed.
+    
+    Parameters
+    ----------
+    r_sh : ndarray
+         ndarray of SH coefficients for the single fiber response function 
+    sh_order : int
+         maximal SH order of the SH representation
+         
+    Returns
+    _______
+    r_rh : ndarray
+         Rotational harmonics coefficients representing the input `r_sh`
+    """
+
+    dirac_sh = gen_dirac(0, 0, sh_order)
+    k = np.nonzero( dirac_sh )[0]
+    r_rh = r_sh[k] / dirac_sh[k]
+    return r_rh
+    
+def gen_dirac( pol, azi, sh_order ) :
+    m, n = sph_harm_ind_list(sh_order)
+    rsh = real_sph_harm
+    # other bases support TO DO
+    
+    dirac = np.zeros( (m.shape) )
+    i = 0
+    for l in np.arange(0,sh_order+1,2) :
+        for m in np.arange(-l,l+1) :
+            if m == 0 :
+                dirac[i] = rsh(0, l, azi, pol)
+
+            i = i + 1
+
+    return dirac
+
+def test_gen_dirac() :
+    dirac = np.array([0.2821, 0, 0, 0.6308, 0, 0, 0, 0, 0, 0, 0.8463, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.0171, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.1631, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    # as from mrtrix 
+
+    assert_array_almost_equal(dirac, gen_dirac(0, 0, 8), 2 )
+    
+
+# TO DO: write a test for the sh_to_rh() function
+#def test_sh_to_rh() :
+    
+
+    
+def sf_to_sh2(sf, sphere, sh_order=4, basis_type=None, smooth=0.0):
+    """ Spherical function to spherical harmonics (SH)
+    
+    Parameters
+    ----------
+    sf : ndarray
+         ndarray of values representing spherical functions on the 'sphere'
+    sphere : Sphere
+          The points on which the sf is defined.
+    sh_order : int, optional
+               Maximum SH order in the SH fit,
+               For `sh_order`, there will be
+               (`sh_order`+1)(`sh_order`_2)/2 SH coefficients
+               (default 4)
+    basis_type : {None, 'mrtrix', 'fibernav'}
+                 None for the default dipy basis,
+                 'mrtrix' for the MRtrix basis, and
+                 'fibernav' for the FiberNavigator basis
+                 (default None)
+   smooth : float, optional
+            Lambda-regularization in the SH fit
+            (default 0.0)
+    
+    Returns
+    _______
+    sh : ndarray
+         SH coefficients representing the input `odf`
+    B :  ndarray
+         SH basis matrix, N x (`sh_order`+1)(`sh_order`_2)/2
+         N is the number of points on the `sphere`
+             
+    """
+    m, n = sph_harm_ind_list(sh_order)
+
+    pol = sphere.theta
+    azi = sphere.phi
+
+    sph_harm_basis = sph_harm_lookup.get(basis_type)
+    if not sph_harm_basis:
+        raise ValueError(' Wrong basis type name ')
+    B = sph_harm_basis(m, n, azi[:, None], pol[:, None])
+    
+    L = -n * (n + 1)
+    invB = smooth_pinv(B, np.sqrt(smooth)*L)
+    R = (sh_order + 1) * (sh_order + 2) / 2
+    sh = np.zeros(sf.shape[:-1] + (R,))
+
+    sh = np.dot(sf, invB.T)        
+
+    return sh,B
+
+
+
+#test_gen_dirac()
+#test_b()
+test_deconv()
 
 
     
