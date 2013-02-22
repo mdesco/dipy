b67e4da76e2c2137aade01d790b43972c94532e1
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-02-22 13:44:26 -0500

    DOC: changed the documentation around csdeconv

--- dipy/reconst/tests/test_odf.py
+++ dipy/reconst/tests/test_odf.py
@@ -206,7 +206,7 @@ def test_sf_to_sh():
     assert_array_almost_equal(odf2d, odf2d_sf, 2)
 
 def test_deconv():
-    SNR = None #10, 20, 30
+    SNR = 30 #None #10, 20, 30
     bvalue = 1000
     S0 = 1
     sh_order = 8
@@ -276,7 +276,7 @@ def test_deconv():
 
     fodf_sh,num_it = csdeconv( r_rh, s_sh, sh_order, B_regul, 1.0, 0.1 )
     fodf = sh_to_sf(fodf_sh, psphere, sh_order)
-    print 'converged afted %d iterations'%num_it
+    print 'converged after %d iterations'%num_it
     visu = True
     if visu :
         odf_gt = multi_tensor_odf( psphere.vertices, [0.5, 0.5], s_mevals, s_mevecs )
@@ -284,9 +284,89 @@ def test_deconv():
         r = fvtk.ren()
         fvtk.add( r, fvtk.sphere_funcs( np.vstack((odf_gt, fodf)), psphere ) )
         fvtk.show( r )
+
+    #fodf_sh,num_it = csdeconv( r_rh, s_sh, sh_order, B_regul, 1.0, 0.1 )
+    #fodf = sh_to_sf(fodf_sh, psphere, sh_order)
+
+
+def odf_csdeconv( r_sh, s_sh, sh_order, B_regul, Lambda, tau ) :
+    """ ODF constrained-regularized sherical deconvolution 
+
+    Parameters
+    ----------
+    r_sh : ndarray
+         ndarray of SH coefficients for the single fiber response function
+    s_sh : ndarray
+         ndarray of SH coefficients for the spherical function to be deconvolved
+    sh_order : int
+         maximal SH order of the SH representation
+    B_regul : ndarray
+         SH basis matrix used for deconvolution
+    Lambda : float
+         lambda parameter in minimization equation (default 1.0)
+    tau : float
+         tau parameter in the L matrix construction (default 0.1)
+         
+    Returns
+    _______
+    fodf_sh : ndarray
+         Spherical harmonics coefficients of the constrained-regularized fiber ODF
+    num_it : int
+         Number of iterations in the constrained-regularization used for convergence
+
+    References
+    ----------
+    Descoteaux, M, et. al. TMI 2009.
+    """
+    m, n = sph_harm_ind_list(sh_order)
+
+    # building forward spherical deconvolution matrix
+    b = np.zeros( (m.shape) )
+    i = 0
+    for l in np.arange(0,sh_order+1,2) :
+        for m in np.arange(-l,l+1) :
+            b[i] = r_rh[l/2]                
+            i = i + 1
+    R = np.diag( b )
+
+    # generate initial fODF estimate, truncated at SH order 4
+    fodf_sh = np.linalg.lstsq(R, s_sh)[0]  # a\b
+    fodf_sh[15:] = 0    
+    
+    #set threshold on FOD amplitude used to identify 'negative' values
+    threshold = tau*np.mean(np.dot(B_regul, fodf_sh));
+    
+    # scale lambda to account for differences in the number of 
+    # SH coefficients and number of mapped directions
+    Lambda = Lambda * R.shape[0] * r_rh[0] / B_regul.shape[0]
+    if sh_order == 8 :
+        assert_almost_equal(Lambda, 0.822627111014, 4) # as in mrtrix
+
+    k = []
+    convergence = 50
+    for num_it in np.arange(1,convergence+1) :
+        A = np.dot(B_regul, fodf_sh)
+        k2 = np.nonzero( A < threshold )[0]
         
+        if (k2.shape[0] + R.shape[0])  < B_regul.shape[1] :
+            print 'too few negative directions identified - failed to converge'
+            return fodf_sh,num_it
+    
+        if num_it > 1 and k.shape[0] == k2.shape[0] :
+            if (k == k2).all() :
+                return fodf_sh,num_it
+           
+        k = k2
+        M = np.concatenate( (R, Lambda*B_regul[k, :] ) )
+        S = np.concatenate( (s_sh, np.zeros( k.shape ) ) )
+        fodf_sh = np.linalg.lstsq(M, S)[0]  # M\S
+        
+    print 'maximum number of iterations exceeded - failed to converge';
+    return fodf_sh,num_it
+
+
 def csdeconv( r_rh, s_sh, sh_order, B_regul, Lambda, tau ) :
-    """ Unfiltered spherical deconvolution 
+    """ Constrained-regularized spherical deconvolution (CSD)
 
     Deconvolves the axially symmetric single fiber response
     function `r_rh` in rotational harmonics coefficients from the spherical function
@@ -298,14 +378,14 @@ def csdeconv( r_rh, s_sh, sh_order, B_regul, Lambda, tau ) :
          ndarray of rotational harmonics coefficients for the single fiber response function
     s_sh : ndarray
          ndarray of SH coefficients for the spherical function to be deconvolved
-    sphere_regul : Sphere
-         The points on which the regularization will take place
+    sh_order : int
+         maximal SH order of the SH representation
+    B_regul : ndarray
+         SH basis matrix used for deconvolution
     Lambda : float
          lambda parameter in minimization equation (default 1.0)
     tau : float
          tau parameter in the L matrix construction (default 0.1)
-    sh_order : int
-         maximal SH order of the SH representation
          
     Returns
     _______
@@ -364,6 +444,7 @@ def csdeconv( r_rh, s_sh, sh_order, B_regul, Lambda, tau ) :
     print 'maximum number of iterations exceeded - failed to converge';
     return fodf_sh,num_it
 
+
 def sdeconv( r_rh, s_sh, sh_order, filter ) :
     """ Unfiltered and filtered spherical deconvolution 
 
@@ -400,7 +481,6 @@ def sdeconv( r_rh, s_sh, sh_order, filter ) :
     ----------
     Tournier, J.D., et. al. NeuroImage 2004.
     """
-    
     m, n = sph_harm_ind_list(sh_order)
     # other bases support TO DO
 
