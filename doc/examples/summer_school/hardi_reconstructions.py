8750c195c18a84c57e95da2a5ed57f2cb0229f51
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-06-04 14:42:40 -0400

    hardi example finished: should add peak visualiztion

--- doc/examples/summer_school/hardi_reconstructions.py
+++ doc/examples/summer_school/hardi_reconstructions.py
@@ -4,18 +4,20 @@ from dipy.data import fetch_stanford_hardi, read_stanford_hardi, get_sphere
 from dipy.reconst.shm import CsaOdfModel, QballModel, sh_to_sf
 from dipy.data import get_sphere
 from dipy.viz import fvtk
+from dipy.reconst.odf import gfa
 
 fetch_stanford_hardi()
 img, gtab = read_stanford_hardi()
 data = img.get_data()
 sphere = get_sphere('symmetric724')
+sphere2 = get_sphere('symmetric362')
 
 data_small  = data[20:50,55:85, 38:39]
 
 
 order = 4
 qballmodel = QballModel(gtab, order, smooth=0.006)
-print 'Computing the CSA ODFs...'
+print 'Computing the qball ODFs...'
 qballfit = qballmodel.fit(data_small)
 qballodfs = qballfit.odf(sphere)
 
@@ -25,22 +27,19 @@ fvtk.add(ren, fvtk.sphere_funcs(qballodfs, sphere, colormap='jet'))
 fvtk.show(ren)
 print('Saving illustration as qballodfs.png')
 fvtk.record(ren, n_frames=1, out_path='qballodfs.png', size=(600, 600))
-fvtk.clear(r)
+fvtk.clear(ren)
 
 
 # min-max normalize ODFs
 print 'Min-max normalizing and visualizing...'
-minmax_odfs = odfs
-fvtk.add(r, fvtk.sphere_funcs(minmax_odfs, sphere, colormap='jet'))
-fvtk.show(r)
+minmax_odfs = (qballodfs - np.min(qballodfs, -1)[..., None]) / (np.max(qballodfs, -1)[..., None] - np.min(qballodfs, -1)[..., None])
+
+fvtk.add(ren, fvtk.sphere_funcs(minmax_odfs, sphere, colormap='jet'))
+fvtk.show(ren)
 
 print('Saving illustration as qball_minmax_odfs.png')
-fvtk.record(r, n_frames=1, out_path='qball_minmax_odfs.png', size=(600, 600))
-fvtk.clear(r)
-
-
-
-
+fvtk.record(ren, n_frames=1, out_path='qball_minmax_odfs.png', size=(600, 600))
+fvtk.clear(ren)
 
 
 csamodel = CsaOdfModel(gtab, order, smooth=0.006)
@@ -48,11 +47,32 @@ print 'Computing the CSA ODFs...'
 csafit = csamodel.fit(data_small)
 csaodfs = csafit.odf(sphere)
 
+print 'Visualizing CSA ODFs...'
 ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(csaodfs, sphere, colormap='jet'))
 fvtk.show(ren)
 print('Saving illustration as csa_odfs.png')
 fvtk.record(ren, n_frames=1, out_path='csa_odfs.png', size=(600, 600))
+fvtk.clear(ren)
 
+# notice the negative values produced
+print 'Visualizing CSA ODFs without negative values...'
+ren = fvtk.ren()
+fvtk.add(ren, fvtk.sphere_funcs(np.clip(csaodfs, 0, np.max(csaodfs, -1)[..., None]), sphere, colormap='jet'))
+fvtk.show(ren)
+print('Saving illustration as csa_odfs_positive.png')
+fvtk.record(ren, n_frames=1, out_path='csa_odfs_positive.png', size=(600, 600))
 
 # TO DO: visualize peaks
+
+mask = data[..., 0] > 50
+qmodel = QballModel(gtab, order, smooth=0.006)
+print 'Computing the ODFs and GFA on the full brain...'
+qfit = qmodel.fit(data, mask) 
+SH_coeff   = qfit._shm_coef
+GFA     = gfa(qfit.odf(sphere2))
+nib.save(nib.Nifti1Image(GFA.astype('float32'), img.get_affine()), 'gfa.nii.gz')    
+nib.save(nib.Nifti1Image(SH_coeff.astype('float32'), img.get_affine()), 'csa_odf_sh.nii.gz')
+
+
+
