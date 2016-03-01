73e7d45d228e5e40f251a57becdc9db4498b5b56
Maxime Descoteaux <maxime.descoteaux@gmail.com>
2016-03-01 05:40:05 -0500

    minor changes

--- bin/dipy_viz_grid.py
+++ bin/dipy_viz_grid.py
@@ -17,7 +17,7 @@ import glob
 verbose=False
 def main():
     print(glob.glob(sys.argv[1]))
-    print(glob.glob(sys.argv[2]))
+    #print(glob.glob(sys.argv[2]))
 
     streamlines = []
     for tracts in glob.glob(sys.argv[1]) :        
@@ -35,8 +35,8 @@ def main():
 
     actors = []
     texts = []
-    for cluster, color, tracts, profile in izip(streamlines, colormap, 
-                                                glob.glob(sys.argv[1]), glob.glob(sys.argv[2])):
+    for cluster, color, tracts in izip(streamlines, colormap, 
+                                       glob.glob(sys.argv[1])) : # glob.glob(sys.argv[2])):
         if verbose :
             print color
 
@@ -50,39 +50,41 @@ def main():
         s = tracts.split("/")
         print(s)
         text = actor.text_3d("Bundle i", font_size=60, 
-                             justification="center", vertical_justification="top")
-        text = actor.text_3d(s[1], font_size=60, 
-                             justification="center", vertical_justification="top")
-        texts.append(text)
-
-        s = profile.split("/")
-        print(profile)
-        p = np.load(profile)
-        #x = np.linspace(0, 1)
-        #p = np.sin(4 * np.pi * x) * np.exp(-5 * x)
-        fig = plt.figure(figsize=(800/300, 500/300), dpi=300)
-        ax = fig.add_subplot(111)
-        ax.set_title("FA tract profile")
-        ax.set_title(s[1])
-        ax.fill_between(np.arange(p[0].shape[0]), 
-                        p[0] - p[1], p[0] + p[1], 
-                        facecolor='y', alpha=0.5)
-                        #facecolor='0.75', alpha=1)
-
-        ax.plot(p[0], color='k', linewidth=5)
-        #ax.fill(p[0], 'r')
-        ax.grid(True)
-        color=(0.5,0.5,0.5)
-        ax.set_axis_bgcolor(color)  
-        arr = utils.matplotlib_figure_to_numpy(fig, dpi=300, transparent=True)
-        plt.close(fig)
-        figure_actor = actor.figure(arr, interpolation='cubic')
-        actors.append(figure_actor)
-        text = actor.text_3d('', font_size=32, 
+                             justification="center", vertical_justification="top")        
+        text = actor.text_3d(s[0], font_size=60, #s[1], font_size=60, 
                              justification="center", vertical_justification="top")
         texts.append(text)
+        
 
-        #ren.add(figure_actor)
+#         if len(profile) != 0 :
+#             s = profile.split("/")
+#             print(profile)
+#             p = np.load(profile)
+#         #x = np.linspace(0, 1)
+#         #p = np.sin(4 * np.pi * x) * np.exp(-5 * x)
+#             fig = plt.figure(figsize=(800/300, 500/300), dpi=300)
+#             ax = fig.add_subplot(111)
+#             ax.set_title("FA tract profile")
+#             ax.set_title(s[1])
+#             ax.fill_between(np.arange(p[0].shape[0]), 
+#                             p[0] - p[1], p[0] + p[1], 
+#                             facecolor='y', alpha=0.5)
+#                         #facecolor='0.75', alpha=1)
+
+#             ax.plot(p[0], color='k', linewidth=5)
+#         #ax.fill(p[0], 'r')
+#             ax.grid(True)
+#             color=(0.5,0.5,0.5)
+#             ax.set_axis_bgcolor(color)  
+#             arr = utils.matplotlib_figure_to_numpy(fig, dpi=300, transparent=True)
+#             plt.close(fig)
+#             figure_actor = actor.figure(arr, interpolation='cubic')
+#             actors.append(figure_actor)
+#             text = actor.text_3d('', font_size=32, 
+#                                  justification="center", vertical_justification="top")
+#             texts.append(text)
+
+#         #ren.add(figure_actor)
 
 
     grid = actor.grid(actors, texts, cell_padding=(50, 100), cell_shape="rect")
